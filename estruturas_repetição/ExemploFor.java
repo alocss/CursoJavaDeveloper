8bd89981f2759dc89518cc2ffe9c58e5e905570a
AlexRibeiro <alexmtt.ribeiro@gmail.com>
2025-03-19 11:06:40 -0300

    Continuação dos estudos sobre Java

---
+++ estruturas_repetição/ExemploFor.java
@@ -0,0 +1,9 @@
+package estruturas_repetição;
+public class ExemploFor {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("i = " + i);
+        }
+        
+    }
+}
---
+++ estruturas_repetição/ExemploForArray.java
@@ -0,0 +1,10 @@
+package estruturas_repetição;
+public class ExemploForArray {
+    public static void main(String[] args) {
+        String alunos [] = {"João", "Maria", "José", "Pedro", "Ana"};
+        for (String aluno : alunos){
+            System.out.println("Nome do aluno é: "+ aluno);
+        }
+    }
+    
+}
