fa5ce8a4de2759ebacb041a12a9af335533dfc9a
AlexRibeiro <alexmtt.ribeiro@gmail.com>
2025-03-19 21:03:32 -0300

    Estrutura de repetição

---
+++ estruturas_repetição/ExemploWhile.java
@@ -0,0 +1,27 @@
+package estruturas_repetição;
+import java.util.concurrent.ThreadLocalRandom; // gerar numeros aleatorios
+
+public class ExemploWhile {
+    public static void main(String[] args) {
+        double mesada = 50.0;
+
+        while (mesada > 0 ) {
+            Double valorDoce = valorAleatorio();
+            /*if (valorDoce > mesada) {
+                valorDoce = mesada; */
+
+                System.out.println("Doce do valor: " + valorDoce + "Adicionado ao carrinho");
+                mesada = mesada - valorDoce;    
+            }
+            System.out.println("Mesada: " + mesada);
+            System.out.println("Joãozinho gastou toda a sua mesada em doces");
+            
+            
+        }
+    
+    private static double valorAleatorio() {
+        return ThreadLocalRandom.current().nextDouble(2,8);
+        
+    }
+   
+}
